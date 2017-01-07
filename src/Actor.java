7e285437ed0b3c4d9baa3eb0ed49d60001f906aa
Lucia <Lucia>
2017-01-07 23:20:59 +0200

    Modified sound mechanism

--- src/Actor.java
+++ src/Actor.java
@@ -3,40 +3,45 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
 /**
  * 
  * 
- * Abstract class used for implementing common functionalities between players and enemies
+ * Abstract class used for implementing common functionalities between players
+ * and enemies
  */
 public abstract class Actor {
 	private String shootingSoundFile = "src/scifi002.wav";
 	protected BufferedImage avatar;
 	private int x = 100, y = 100, step = 10;
-	
-	
+
 	public Actor() {
 	}
+
 	/**
 	 * Plays the actor's shooting sound
 	 */
 	public void playShoothingSound() {
 		try {
-			AudioPlayer.player.start(new AudioStream(new FileInputStream(shootingSoundFile)));
+			File f = new File(shootingSoundFile);
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(f.toURI().toURL());
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioIn);
+			clip.start();
 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
-
 	public BufferedImage getImage() {
 		return avatar;
 	}
@@ -73,17 +78,18 @@ public abstract class Actor {
 	public void setStep(int step) {
 		this.step = step;
 	}
+
 	public void setShootingSoundFile(String shootingSoundFile) {
 		this.shootingSoundFile = shootingSoundFile;
-		
+
 	}
+
 	public BufferedImage getAvatar() {
 		return avatar;
 	}
+
 	public void setAvatar(BufferedImage avatar) {
 		this.avatar = avatar;
 	}
-	
-	
 
 }
